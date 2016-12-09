c81ebdbcb9124c558274117d7b98cabb8f0ccbc8
Admin <Admin@GABO-PC>
2016-12-09 01:59:49 +0100

    Add some empty projects

---
+++ wMeasuringTape/Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PEPlugin;
+
+namespace wMeasuringTape
+{
+    public class Main : IPEPlugin
+    {
+        public void Run(IPERunArgs args)
+        {
+
+        }
+
+        public string Name { get { return "wMeasuringTape"; } }
+
+        public string Description { get { return "Measure the distance between two points"; } }
+
+        private class Opt : IPEPluginOption
+        {
+            public string RegisterMenuText { get { return "wMeasuringTape"; } }
+            public bool RegisterMenu { get { return true; } }
+            public bool Bootup { get { return false; } }
+        }
+
+        public IPEPluginOption Option { get { return new Opt(); } }
+
+
+        public string Version
+        {
+            get
+            {
+                return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public void Dispose()
+        {
+            ;
+        }
+    }
+}
---
+++ wMeasuringTape/MeasuringTapeForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace wMeasuringTape
+{
+    partial class MeasuringTapeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.toList = new System.Windows.Forms.ListBox();
+            this.fromList = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.fromList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.toList);
+            this.splitContainer1.Size = new System.Drawing.Size(293, 182);
+            this.splitContainer1.SplitterDistance = 143;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // toList
+            // 
+            this.toList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toList.FormattingEnabled = true;
+            this.toList.Location = new System.Drawing.Point(0, 0);
+            this.toList.Name = "toList";
+            this.toList.Size = new System.Drawing.Size(146, 182);
+            this.toList.TabIndex = 0;
+            // 
+            // fromList
+            // 
+            this.fromList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fromList.FormattingEnabled = true;
+            this.fromList.Location = new System.Drawing.Point(0, 0);
+            this.fromList.Name = "fromList";
+            this.fromList.Size = new System.Drawing.Size(143, 182);
+            this.fromList.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 211);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 59);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // MeasuringTapeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 281);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MeasuringTapeForm";
+            this.Text = "MeasuringTapeForm";
+            this.Load += new System.EventHandler(this.MeasuringTapeForm_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox fromList;
+        private System.Windows.Forms.ListBox toList;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
