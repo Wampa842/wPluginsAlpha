efb0b3d023ca53dc17e380788a854747626c32a9
Admin <Admin@GABO-PC>
2017-02-26 21:38:44 +0100

    update

--- wObjIO/ObjExport.cs
+++ wObjIO/ObjExport.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.ComponentModel;
 
 using PEPlugin;
 using PEPlugin.SDX;
@@ -11,7 +13,224 @@ using PEPlugin.Pmx;
 
 namespace wObjIO
 {
-    class wObjExport : IPEExportPlugin
+    public struct ObjExportSettings
+    {
+        public float ScaleX { get; set; }
+        public float ScaleY { get; set; }
+        public float ScaleZ { get; set; }
+        public bool SwapAxes { get; set; }
+        //public bool ReverseFaces { get; set; }
+        public bool MirrorU { get; set; }
+        public bool MirrorV { get; set; }
+    }
+
+
+    public class ObjFileExport
+    {
+        //Mesh data
+        public class WFace
+        {
+            public int[] V, Vt, Vn;
+            public WFace()
+            {
+                V = new int[3];
+                Vt = new int[3];
+                Vn = new int[3];
+            }
+            public void Vert1(int v, int vt, int vn)
+            {
+                this.V[0] = v;
+                this.Vt[0] = vt;
+                this.Vn[0] = vn;
+            }
+            public void Vert2(int v, int vt, int vn)
+            {
+                this.V[1] = v;
+                this.Vt[1] = vt;
+                this.Vn[1] = vn;
+            }
+            public void Vert3(int v, int vt, int vn)
+            {
+                this.V[2] = v;
+                this.Vt[2] = vt;
+                this.Vn[2] = vn;
+            }
+
+            public string FaceToString()
+            {
+                string v1 = V[0].ToString() + '/' + Vt[0].ToString() + '/' + Vn[0].ToString();
+                string v2 = V[1].ToString() + '/' + Vt[1].ToString() + '/' + Vn[1].ToString();
+                string v3 = V[2].ToString() + '/' + Vt[2].ToString() + '/' + Vn[2].ToString();
+                return "f " + v1 + ' ' + v2 + ' ' + v3;
+            }
+        }
+        public class WMaterial
+        {
+            public List<WFace> Faces;
+            public V3 Diffuse;
+            public V3 Ambient;
+            public V3 Specular;
+            public float Glossiness = 32;
+            public float Opacity = 1.0f;
+            public string Bitmap = "";
+            public string Name = "";
+            public WMaterial(IPXMaterial source)
+            {
+                Faces = new List<WFace>();
+                Name = source.Name;
+                Diffuse = source.Diffuse.ToV3();
+                Opacity = source.Diffuse.A;
+                Ambient = source.Ambient;
+                Specular = source.Specular;
+                Glossiness = source.Power;
+                if (!string.IsNullOrWhiteSpace(source.Tex))
+                    Bitmap = source.Tex;
+            }
+        }
+        public int ItemCount
+        {
+            get
+            {
+                int faceCount = 0;
+                foreach(WMaterial m in materialList)
+                {
+                    faceCount += m.Faces.Count;
+                }
+                return faceCount + vList.Count + vtList.Count + vnList.Count;
+            }
+        }
+
+        private List<string> vList;
+        private List<string> vtList;
+        private List<string> vnList;
+        private List<WMaterial> materialList;
+
+        public ObjFileExport(IPXPmx pmx, ObjExportSettings settings)
+        {
+            List<IPXVertex> processedVerts = new List<IPXVertex>();
+            vList = new List<string>();
+            vtList = new List<string>();
+            vnList = new List<string>();
+            materialList = new List<WMaterial>();
+            //Add vertices to the lists
+            foreach(IPXVertex v in pmx.Vertex)
+            {
+                string vStr = "v " + v.Position.X.ToString() + ' ' + v.Position.Y.ToString() + ' ' + v.Position.Z.ToString();
+                string vtStr = "vt " + v.UV.U.ToString() + ' ' + (-v.UV.V).ToString() + ' ' + 0.0f.ToString();
+                string vnStr = "vn " + v.Normal.X.ToString() + ' ' + v.Normal.Y.ToString() + ' ' + v.Normal.Z.ToString();
+
+                if (!vList.Contains(vStr)) vList.Add(vStr);
+                if (!vtList.Contains(vtStr)) vtList.Add(vtStr);
+                if (!vnList.Contains(vnStr)) vnList.Add(vnStr);
+            }
+            //Process the scene by iterating over its materials, then its faces. Relevant data will be written into strings.
+            foreach(IPXMaterial m in pmx.Material)
+            {
+                WMaterial mat = new WMaterial(m);
+                foreach(IPXFace f in m.Faces)
+                {
+                    WFace face = new WFace();
+                    //Vertex 1
+                    IPXVertex v = f.Vertex1;
+                    string vStr = "v " + v.Position.X.ToString() + ' ' + v.Position.Y.ToString() + ' ' + v.Position.Z.ToString();
+                    string vtStr = "vt " + v.UV.U.ToString() + ' ' + (-v.UV.V).ToString() + ' ' + 0.0f.ToString();
+                    string vnStr = "vn " + v.Normal.X.ToString() + ' ' + v.Normal.Y.ToString() + ' ' + v.Normal.Z.ToString();
+                    face.Vert1(vList.FindIndex(a => a == vStr) + 1, vtList.FindIndex(a => a == vtStr) + 1, vnList.FindIndex(a => a == vnStr) + 1);
+
+                    //Vertex 2
+                    v = f.Vertex2;
+                    vStr = "v " + v.Position.X.ToString() + ' ' + v.Position.Y.ToString() + ' ' + v.Position.Z.ToString();
+                    vtStr = "vt " + v.UV.U.ToString() + ' ' + (-v.UV.V).ToString() + ' ' + 0.0f.ToString();
+                    vnStr = "vn " + v.Normal.X.ToString() + ' ' + v.Normal.Y.ToString() + ' ' + v.Normal.Z.ToString();
+                    face.Vert2(vList.FindIndex(a => a == vStr) + 1, vtList.FindIndex(a => a == vtStr) + 1, vnList.FindIndex(a => a == vnStr) + 1);
+                    
+                    //Vertex 3
+                    v = f.Vertex3;
+                    vStr = "v " + v.Position.X.ToString() + ' ' + v.Position.Y.ToString() + ' ' + v.Position.Z.ToString();
+                    vtStr = "vt " + v.UV.U.ToString() + ' ' + (-v.UV.V).ToString() + ' ' + 0.0f.ToString();
+                    vnStr = "vn " + v.Normal.X.ToString() + ' ' + v.Normal.Y.ToString() + ' ' + v.Normal.Z.ToString();
+                    face.Vert3(vList.FindIndex(a => a == vStr) + 1, vtList.FindIndex(a => a == vtStr) + 1, vnList.FindIndex(a => a == vnStr) + 1);
+                    
+                    mat.Faces.Add(face);
+                }
+                materialList.Add(mat);
+            }
+        }
+
+        public void SaveMtl(string objPath, ObjExportProgress progress)
+        {
+            using (StreamWriter mtl = File.CreateText(Path.Combine(Path.GetDirectoryName(objPath), Path.GetFileNameWithoutExtension(objPath) + ".mtl")))
+            {
+                string mtlStr = string.Empty;
+                mtlStr += "# wObjIO OBJ Exporter - (c) 2017 Wampa842\n# wampa842.deviantart.com\n# wampa842@gmail.com\n\n# File created: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm") + "\n";
+
+                foreach (WMaterial m in materialList)
+                {
+                    progress.progressBar1.Value++;
+                    mtlStr += "\n\nnewmtl " + m.Name;
+                    mtlStr += "\n    Ka " + m.Ambient.R + ' ' + m.Ambient.G + ' ' + m.Ambient.B;
+                    mtlStr += "\n    Ks " + m.Specular.R + ' ' + m.Specular.G + ' ' + m.Specular.B;
+                    mtlStr += "\n    Kd " + m.Diffuse.R + ' ' + m.Diffuse.G + ' ' + m.Diffuse.B;
+                    mtlStr += "\n    map_Ka " + m.Bitmap;
+                    mtlStr += "\n    map_Kd " + m.Bitmap;
+                    mtlStr += "\n    d " + m.Opacity;
+                    mtlStr += "\n    Ns " + m.Glossiness;
+                    mtlStr += "\n    illum 2";
+                }
+
+                mtl.Write(mtlStr);
+                mtl.Close();
+            }
+        }
+
+        public void SaveObj(string objPath, ObjExportProgress progress)
+        {
+            SaveMtl(objPath, progress);
+            using (StreamWriter obj = new StreamWriter(objPath))
+            {
+                obj.WriteLine("# wObjIO OBJ Exporter - (c) 2017 Wampa842");
+                obj.WriteLine("# wampa842.deviantart.com");
+                obj.WriteLine("# wampa842@gmail.com");
+
+                obj.WriteLine("\n\nmtllib " + Path.GetFileNameWithoutExtension(objPath) + ".mtl\n");
+
+                //Write vertices
+                foreach (string v in vList)
+                {
+                    progress.progressBar1.Value++;
+                    obj.WriteLine(v);
+                }
+                obj.WriteLine("# " + vList.Count + " vertex positions\n");
+                foreach (string vt in vtList)
+                {
+                    progress.progressBar1.Value++;
+                    obj.WriteLine(vt);
+                }
+                obj.WriteLine("# " + vtList.Count + " texture coordinates\n");
+                foreach (string vn in vnList)
+                {
+                    progress.progressBar1.Value++;
+                    obj.WriteLine(vn);
+                }
+                obj.WriteLine("# " + vnList.Count + " vertex normals\n");
+
+                //Groups
+                foreach (WMaterial m in materialList)
+                {
+                    obj.WriteLine("\ng " + m.Name);
+                    obj.WriteLine("usemtl " + m.Name);
+                    obj.WriteLine("s 1");
+                    foreach(WFace f in m.Faces)
+                    {
+                        obj.WriteLine(f.FaceToString());
+                    }
+                }
+            }
+
+        }
+    }
+    
+    public class wObjExport : IPEExportPlugin
     {
         public string Ext { get { return ".obj"; } }
         public string Caption { get { return "Wavefront OBJ (wObjIO Exporter)"; } }
@@ -32,8 +251,24 @@ namespace wObjIO
                 Each face will be a triangle. A face will consist of three vertex definitions.
                 Each vertex definition will contain the 1-based index of its v, vt and vn entry, separated by forward slashes, definitions separated by spaces.
 
+                The MTL file contains material definitions.
+                Processing a material will append a newmtl and relevant data to the end of the file.
+                The material name is as it appears in PMX.
+                Kd, Ks, Ns, Ka, d are float values. map_Kd and map_Ka are both the diffuse map name.
+
                 I might also include copyright and meta information as comments.
                 */
+
+                ObjExportSettingsForm settingsForm = new ObjExportSettingsForm();
+                if (settingsForm.ShowDialog() == DialogResult.OK)
+                {
+                    ObjExportProgress report = new ObjExportProgress();
+                    report.Show();
+                    ObjFileExport obj = new ObjFileExport(pmx, settingsForm.Settings);
+                    report.progressBar1.Maximum = obj.ItemCount;
+                    obj.SaveObj(path, report);
+                    report.Close();
+                }
             }
             catch(Exception ex)
             {
